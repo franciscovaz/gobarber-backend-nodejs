c01b6ce8a4c6198df52868d4cee9e933aff8be75
Francisco Vaz <ftomasvaz@gmail.com>
2020-05-09 17:45:17 +0100

    IMailProvider

---
+++ src/modules/users/services/SendForgotPasswordEmailService.spec.ts
@@ -0,0 +1,24 @@
+// import AppError from '@shared/errors/AppError';
+
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import CreateUserService from './CreateUserService';
+
+describe('CreateUser', () => {
+  it('should be able to create a new user', async () => {
+    const fakeUserRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+    const createUser = new CreateUserService(
+      fakeUserRepository,
+      fakeHashProvider,
+    );
+
+    const user = await createUser.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    expect(user).toHaveProperty('id');
+  });
+});
