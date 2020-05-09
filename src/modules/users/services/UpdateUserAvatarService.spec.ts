4198702ad8f7dc25f1ec42f3b0f02233a610d6f3
Francisco Vaz <ftomasvaz@gmail.com>
2020-05-09 18:37:11 +0100

    TEST: should not be able to recover a non-existing user password

--- src/modules/users/services/UpdateUserAvatarService.spec.ts
+++ src/modules/users/services/UpdateUserAvatarService.spec.ts
@@ -37,7 +37,7 @@ describe('UpdateUserAvatar', () => {
       fakeStorageProvider,
     );
 
-    expect(
+    awaitexpect(
       updateUserAvatar.execute({
         user_id: 'non-existing-user',
         avatarFilename: 'avatar.jpg',
