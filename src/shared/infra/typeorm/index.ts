f9cf5c608d7860a14a81ee5f64a33a3c389ea08e
Francisco Vaz <ftomasvaz@gmail.com>
2020-05-14 07:33:59 +0100

    add mongo  and config

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,4 +1,4 @@
-import { createConnection } from 'typeorm';
+import { createConnections } from 'typeorm';
 
 // com esta função, vai a todo o projeto ver se existe o ficheiro ormconfig.json e corre as configurações
-createConnection();
+createConnections();
