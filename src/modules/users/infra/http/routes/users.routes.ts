9bf0be6f47f2bba2e6db981555328a66d1679569
Francisco Vaz <ftomasvaz@gmail.com>
2020-05-24 18:43:43 +0100

    fix validation on create user

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -22,7 +22,7 @@ usersRouter.post(
   celebrate({
     [Segments.BODY]: {
       name: Joi.string().required(),
-      email: Joi.string().email().required,
+      email: Joi.string().email().required(),
       password: Joi.string().required(),
     },
   }),
