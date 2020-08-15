33991b58cf599a5a4baf913897ae84d09edb1743
Francisco Vaz <ftomasvaz@gmail.com>
2020-08-15 09:23:58 +0100

    remove comments to clear the cash

--- src/modules/appointments/services/ListProvidersService.ts
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -21,10 +21,9 @@ class ListProvidersService {
   ) {}
 
   public async execute({ user_id }: IRequest): Promise<User[]> {
-    /* let users = await this.cacheProvider.recover<User[]>(
+    let users = await this.cacheProvider.recover<User[]>(
       `providers-list: ${user_id}`,
-    ); */
-    let usersç;
+    );
 
     if (!users) {
       users = await this.usersRepository.findAllProviders({
